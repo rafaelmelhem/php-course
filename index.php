6933d85bd25af3e7022236a25187381303fa7745
Rafael Melhem <rafael.melhem@gmail.com>
2020-05-08 01:36:14 +0000

    add tax calc function

--- index.php
+++ index.php
@@ -17,13 +17,24 @@
 	$products['Toaster']=75;
 	foreach($products as $key => $value) {
 	echo "<p> The ".$key." costs $".$value." </p>";
-} 
+}
 	echo "<h2>Items you can afford</h2>";
 	foreach($products as $key => $value) {
 	if($value <= $credit) {
 	echo "<p> ".$key." </p>";
 	}
 }
+	$amount=800;
+	$taxRate=0.0825;
+	$addedTax= $amount*$taxRate;  //amount = 800, tax = .0825
+	echo $addedTax;
+	
+	function tax_calc($amount,$tax) { 
+	$calculate_tax = $amount*$tax;
+	$amount = round($amount+$calculate_tax,2);
+	return $amount;
+} 
+	echo tax_calc(750,0.223);
 ?>
   </body>
 </html>
